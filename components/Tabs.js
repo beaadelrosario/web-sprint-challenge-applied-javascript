04884370246f1526bc0d3be85420809e31fb659d
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-07-10 08:50:32 -0700

    tabs- axios.get- still figuring it out

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,37 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+// const data = {
+//     "topics": [
+//         "javascript",
+//         "bootstrap",
+//         "technology",
+//         "jquery",
+//         "node.js"
+//     ]
+// }
+
+//tab making function
+function tabMaker(tab){
+    let tabber = document.createElement('div')
+    tabber.classList.add('tab')
+    tabber.textContent = tab
+
+    return tab
+}
+console.log(tabMaker)
+
+//iteration for each topic
+let topics = document.querySelector('.topics')
+console.log(topics)
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(res => { //res is my function
+        res.data.topics.forEach( makeOwnTopicTab => { //always add data
+            topics.appendChild(tabMaker(makeOwnTopicTab))
+        })
+    })
+    .catch(error => { //error function logs error in console
+        console.log(error)
+    })
